b21b055da58a64a491cb6945ca081fcf8135d743
blacksalman <salman51180189@gmail.com>
2022-01-31 19:50:49 +0530

    This is my first published packages:

---
+++ root.js
@@ -0,0 +1,8 @@
+var {add, multiply}  = require('lion-lib-21-lib');
+var result1 = add(1, 2);
+var result2 = multiply(6, 2);
+console.log(result1);
+console.log(result2);
+
+module.exports = add;
+module.exports = multiply;
